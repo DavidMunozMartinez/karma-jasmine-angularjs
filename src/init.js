be38ba627ea8acedaed760df0e469bd394abe1ed
DavidMunozMartinez <jdavid_9502@outlook.com>
2020-03-07 14:35:50 -0600

    Added configuration object validator before initializing all the mock process

--- src/init.js
+++ src/init.js
@@ -1,206 +1,242 @@
 (function (window) {
-    'use strict';
-    /**Unit tests utilities library for creating and providing all necessary data for angularjs unit test setup */
+	'use strict';
+	/**Unit tests utilities library for creating and providing all necessary data for angularjs unit test setup */
 
-    /**Setup global variable for the testbed */
-    window.TestBed = {
-        configure: configure
-    };
+	/**Setup global variable for the testbed */
+	window.TestBed = {
+		configure: configure
+	};
 
-    // pollify for startsWith in IE
-    if (!String.prototype.startsWith) {
-        String.prototype.startsWith = function (searchString, position) {
-            position = position || 0;
-            return this.substr(position, searchString.length) === searchString;
-        };
-    }
+	// pollify for startsWith in IE
+	if (!String.prototype.startsWith) {
+		String.prototype.startsWith = function (searchString, position) {
+			position = position || 0;
+			return this.substr(position, searchString.length) === searchString;
+		};
+	}
 
-    /**
-     * Creates a new controller instance of a given controller name
-     * @param {injector} injector AngularJS injector service used to get the necessary services to instantiate a new controller
-     * @param {String} ctrlName The name of the controller we want to instantiate
-     */
-    function createController(injector, ctrlName) {
-        var $rootScope = injector.get('$rootScope');
-        var $controller = injector.get('$controller');
-        var $scope = $rootScope.$new();
-        var controller = $controller(ctrlName, {
-            $scope: $scope
-        });
+	/**
+	 * Creates a new controller instance of a given controller name
+	 * @param {injector} injector AngularJS injector service used to get the necessary services to instantiate a new controller
+	 * @param {String} ctrlName The name of the controller we want to instantiate
+	 */
+	function createController(injector, ctrlName) {
+		var $rootScope = injector.get('$rootScope');
+		var $controller = injector.get('$controller');
+		var $scope = $rootScope.$new();
+		var controller = $controller(ctrlName, {
+			$scope: $scope
+		});
 
-        return {
-            controller: controller,
-            scope: $scope
-        };
-    }
+		return {
+			controller: controller,
+			scope: $scope
+		};
+	}
 
-    function createDirective(injector, directiveDefinition) {
-        var $rootScope = injector.get('$rootScope');
-        var $compile = injector.get('$compile');
-        var $scope = $rootScope.new();
-        if (directiveDefinition.$scope) {
-            var keys = Object.keys(directiveDefinition.$scope);
-            keys.forEach(function (key) {
-                $scope[key] = directiveDefinition.$scope[key];
-            });
-        }
-        if (!directiveDefinition.selector) {
-            throw 'Please provide a selector in directive.selector';
-        }
-        var directive = $compile(directiveDefinition.selector)($scope);
-        return {
-            directive: directive,
-            scope: $scope
-        };
-    }
+	function createDirective(injector, directiveDefinition) {
+		var $rootScope = injector.get('$rootScope');
+		var $compile = injector.get('$compile');
+		var $scope = $rootScope.new();
+		if (directiveDefinition.$scope) {
+			var keys = Object.keys(directiveDefinition.$scope);
+			keys.forEach(function (key) {
+				$scope[key] = directiveDefinition.$scope[key];
+			});
+		}
+		if (!directiveDefinition.selector) {
+			throw 'Please provide a selector in directive.selector';
+		}
+		var directive = $compile(directiveDefinition.selector)($scope);
+		return {
+			directive: directive,
+			scope: $scope
+		};
+	}
+
+	function invalidConfigObj(options) {
+		var components = [];
+		var missingObligatories = [];
+		var obligatory = ['module'];
+
+		obligatory.forEach(function (prop) {
+			if (!options[prop]) {
+				missingObligatories.push(prop);
+			}
+		});
+
+		if (missingObligatories > 0) {
+			var message = 'Missing';
+			missingObligatories.forEach(function (prop) {
+				message += ' ' + prop;
+			});
+			return message + ' in configuration object.'
+		}
+
+		if (options.service) { components.push('service'); }
+		if (options.factory) { components.push('factory'); }
+		if (options.controller) { components.push('controller'); }
+		if (options.directive) { components.push('directive'); }
+
+		if (components > 1) {
+			return 'Cannot provide multiple component types in the config object, you can only provide either "service", "factory", "controller" or "directive"';
+		}
+
+		return false;
+	}
 
 
-    /**
-     * Creates jasmine spies and provides them over a modifier function to
-     * @param {Object} data object with keys that represent the spy name and the values represent the spy methods or values
-     * @param {(name: String, value: any)=> {}} modifier Optional modifier function that will be executed over every new jasmine spy, returns
-     * the spy name and value assigned to it
-     */
-    function buildJasmineSpies(data, modifier) {
-        var keys = Object.keys(data);
-        keys.forEach(function (key) {
-            var value = data[key];
-            var spy = buildJasmineSpy(key, value);
-            if (modifier) {
-                modifier(key, spy);
-            }
-        });
-    }
+	/**
+	 * Creates jasmine spies and provides them over a modifier function to
+	 * @param {Object} data object with keys that represent the spy name and the values represent the spy methods or values
+	 * @param {(name: String, value: any)=> {}} modifier Optional modifier function that will be executed over every new jasmine spy, returns
+	 * the spy name and value assigned to it
+	 */
+	function buildJasmineSpies(data, modifier) {
+		var keys = Object.keys(data);
+		keys.forEach(function (key) {
+			var value = data[key];
+			var spy = buildJasmineSpy(key, value);
+			if (modifier) {
+				modifier(key, spy);
+			}
+		});
+	}
 
-    /**
-     * Creates a jasmine spy that will be used as a fake dependency for the service we are trying to instantiate
-     * @param {String} name jasmine spy name
-     * @param {any} values Value that will be assigned to the created jasmine spy
-     */
-    function buildJasmineSpy(name, values) {
-        // For more on jasmine spies see: https://jasmine.github.io/api/edge/Spy
-        var jasmineSpyObj = null;
-        // If its an empty object it means that we are not using any method of this dependency
-        if (jQuery.isEmptyObject(values)) {
-            jasmineSpyObj = jasmine.createSpy(name);
-        }
+	/**
+	 * Creates a jasmine spy that will be used as a fake dependency for the service we are trying to instantiate
+	 * @param {String} name jasmine spy name
+	 * @param {any} values Value that will be assigned to the created jasmine spy
+	 */
+	function buildJasmineSpy(name, values) {
+		// For more on jasmine spies see: https://jasmine.github.io/api/edge/Spy
+		var jasmineSpyObj = null;
+		// If its an empty object it means that we are not using any method of this dependency
+		if (jQuery.isEmptyObject(values)) {
+			jasmineSpyObj = jasmine.createSpy(name);
+		}
 
-        // If its not empty then we have methods to mock for this dependency
-        else {
-            jasmineSpyObj = jasmine.createSpyObj(name, values);
-            var valuesKey = Object.keys(values);
+		// If its not empty then we have methods to mock for this dependency
+		else {
+			jasmineSpyObj = jasmine.createSpyObj(name, values);
+			var valuesKey = Object.keys(values);
 
-            /**For each method that we are trying to mock we use the "callFake" method from jasmine spy objects
-             * so that we can control the behavior if there is any defined */
-            valuesKey.forEach(function (valueKey) {
-                var value = values[valueKey];
-                if (value) {
-                    if (typeof value === 'function') {
-                        jasmineSpyObj[valueKey].and.callFake(value);
-                    }
-                    else {
-                        jasmineSpyObj[valueKey] = value;
-                    }
-                }
-            });
-        }
-        return jasmineSpyObj;
-    }
+			/**For each method that we are trying to mock we use the "callFake" method from jasmine spy objects
+			 * so that we can control the behavior if there is any defined */
+			valuesKey.forEach(function (valueKey) {
+				var value = values[valueKey];
+				if (value) {
+					if (typeof value === 'function') {
+						jasmineSpyObj[valueKey].and.callFake(value);
+					}
+					else {
+						jasmineSpyObj[valueKey] = value;
+					}
+				}
+			});
+		}
+		return jasmineSpyObj;
+	}
 
-    /**
-     *
-     * @param {Object} options options object that will contain all the configuration configuration for the TestBed
-     * @param {String} options.module name of the module that will be mocked for the testbed
-     * @param {String} options.service name of the service that will be injected for the testBed
-     * @param {String} options.controller name of the controller that will be injected for the testBed
-     * @param {Object} options.dependencies object that will contain all data that will be used to create fake dependencies
-     * or provide angular services to our testbed
-     * @param {Array<String>} options.dependencies.provide Angular services to inject in our testbed
-     * @param {Object} options.dependencies.mock Fake dependencies and methods to mock that will be provided in our testbed
-     */
-    function configure(options) {
-        var injector = null;
-        var provider = null;
-        var dependencies = {
-            mocked: {},
-            provided: {}
-        };
+	/**
+	 *
+	 * @param {Object} options options object that will contain all the configuration configuration for the TestBed
+	 * @param {String} options.module name of the module that will be mocked for the testbed
+	 * @param {String} options.service name of the service that will be injected for the testBed
+	 * @param {String} options.controller name of the controller that will be injected for the testBed
+	 * @param {Object} options.dependencies object that will contain all data that will be used to create fake dependencies
+	 * or provide angular services to our testbed
+	 * @param {Array<String>} options.dependencies.provide Angular services to inject in our testbed
+	 * @param {Object} options.dependencies.mock Fake dependencies and methods to mock that will be provided in our testbed
+	 */
+	function configure(options) {
+		var injector = null;
+		var provider = null;
+		var dependencies = {
+			mocked: {},
+			provided: {}
+		};
 
-        // Defining our module
-        module(options.module, function (_$provide_) {
-            provider = _$provide_;
-        });
+		var invalid = invalidConfigObj(options);
+		if (invalid) {
+			throw invalid;
+		}
 
-        // Handle dependencies
-        if (options.dependencies) {
-            var mock = options.dependencies.mock;
-            var provide = options.dependencies.provide || [];
-            inject(function ($injector) {
-                injector = $injector;
-                // Provide actual angular services to our environment here
-                provide.forEach(function (dependency) {
-                    dependencies.provided[dependency] = injector.get(dependency);
-                });
-            });
-            if (mock) {
-                buildJasmineSpies(options.dependencies.mock, function (name, spy) {
-                    dependencies.mocked[name] = spy;
-                    provider.factory(name, function () {
-                        return spy;
-                    });
-                });
-            }
-        }
+		// Defining our module
+		module(options.module, function (_$provide_) {
+			provider = _$provide_;
+		});
 
-        // Define the test utils object
-        var testUtils = {
-            $provider: provider,
-            $injector: injector,
-            provided: dependencies.provided,
-            mocked: dependencies.mocked,
-            get: injector.get
-        };
+		// Handle dependencies
+		if (options.dependencies) {
+			var mock = options.dependencies.mock;
+			var provide = options.dependencies.provide || [];
+			inject(function ($injector) {
+				injector = $injector;
+				// Provide actual angular services to our environment here
+				provide.forEach(function (dependency) {
+					dependencies.provided[dependency] = injector.get(dependency);
+				});
+			});
+			if (mock) {
+				buildJasmineSpies(options.dependencies.mock, function (name, spy) {
+					dependencies.mocked[name] = spy;
+					provider.factory(name, function () {
+						return spy;
+					});
+				});
+			}
+		}
 
-        if (options.service) {
-            try {
-                testUtils.service = injector.get(options.service);
-            }
-            catch (e) {
-                throw 'Failed to inject service: ' + options.service + '\n' + e;
-            }
-        }
+		// Define the test utils object
+		var testUtils = {
+			$provider: provider,
+			$injector: injector,
+			provided: dependencies.provided,
+			mocked: dependencies.mocked,
+			get: injector.get
+		};
 
-        if (options.factory) {
-            try {
-                testUtils.factory = injector.get(options.factory);
-            }
-            catch (e) {
-                throw 'Failed to inject factory: ' + options.factory + '\n' + e;
-            }
-        }
+		if (options.service) {
+			try {
+				testUtils.service = injector.get(options.service);
+			}
+			catch (e) {
+				throw 'Failed to inject service: ' + options.service + '\n' + e;
+			}
+		}
 
-        if (options.controller) {
-            try {
-                var ctrl = createController(injector, options.controller);
-                testUtils.controller = ctrl.controller;
-                testUtils.$scope = ctrl.scope;
-            }
-            catch (e) {
-                throw 'Failed to inject controller: ' + options.controller + '\n' + e;
-            }
-        }
+		if (options.factory) {
+			try {
+				testUtils.factory = injector.get(options.factory);
+			}
+			catch (e) {
+				throw 'Failed to inject factory: ' + options.factory + '\n' + e;
+			}
+		}
 
-        // if (options.directive) {
-        //     try {
-        //         var result = createDirective(injector, options.directive);
-        //         testUtils.directive = result.directive;
-        //         testUtils.$scope = result.scope;
-        //     }
-        //     catch (e) {
-        //         throw 'Failed to compile directive: ' + options.directive.selector + '\n' + e;
-        //     }
-        // }
+		if (options.controller) {
+			try {
+				var ctrl = createController(injector, options.controller);
+				testUtils.controller = ctrl.controller;
+				testUtils.$scope = ctrl.scope;
+			}
+			catch (e) {
+				throw 'Failed to inject controller: ' + options.controller + '\n' + e;
+			}
+		}
 
-        return testUtils;
-    }
+		// if (options.directive) {
+		//     try {
+		//         var result = createDirective(injector, options.directive);
+		//         testUtils.directive = result.directive;
+		//         testUtils.$scope = result.scope;
+		//     }
+		//     catch (e) {
+		//         throw 'Failed to compile directive: ' + options.directive.selector + '\n' + e;
+		//     }
+		// }
+
+		return testUtils;
+	}
 })(window);
